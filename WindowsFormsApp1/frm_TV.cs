b684419aa4ea6be86739c7d8380798219059f863
huynguyenle2003 <huynguyenle2003@gmail.com>
2023-04-04 18:24:21 +0700

    hi update tvb

--- WindowsFormsApp1/frm_TV.cs
+++ WindowsFormsApp1/frm_TV.cs
@@ -1,9 +1,11 @@
 ﻿using Gma.UserActivityMonitor;
+using KAutoHelper;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -126,12 +128,9 @@ namespace WindowsFormsApp1
                 //HookManager.KeyDown += HookManager_KeyDown;
 
                 HookManager.KeyDown -= HookManager_KeyDown;
-                if (continuedSpame == true || e.KeyCode == Keys.D2)
+                Point current = GetCursorPoint();
+                if (e.KeyCode == Keys.D2)
                 {
-                    //targetDHT = GetCursorPoint();
-                    //targetX2 = new Point(0, 0);
-                    //dht = true;
-                    Point current = GetCursorPoint();
 
                     int diff_y = current.Y - myTVB.Y;
                     int diff_x = current.X - myTVB.X;
@@ -168,7 +167,7 @@ namespace WindowsFormsApp1
                     else
                     {
                         HuyKeyPress(KeyDirectX.F2);
-                        Thread.Sleep(200);
+                        Thread.Sleep(100);
                         SetCursorPos(targetTT.X, targetTT.Y);
                         continuedSpame = true;
                         new Thread(() =>
@@ -179,18 +178,27 @@ namespace WindowsFormsApp1
                 }
                 else
                 {
-                    HuyKeyPress(KeyDirectX.F2);
-                    Thread.Sleep(100);
+                    if (continuedSpame == true)
+                    {
+                        targetX2 = new Point(0, 0);
+                        targetDHT = current;
+                        dht = true;
+                    }
+                    else
+                    {
+                        HuyKeyPress(KeyDirectX.F2);
+                        Thread.Sleep(100);
 
-                    new Thread(() =>
-                    {
-                        GetX2Point();
-                    }).Start();
-                    continuedSpame = true;
-                    new Thread(() =>
-                    {
-                        spam9x();
-                    }).Start();
+                        new Thread(() =>
+                        {
+                            GetX2Point();
+                        }).Start();
+                        continuedSpame = true;
+                        new Thread(() =>
+                        {
+                            spam9x();
+                        }).Start();
+                    }
                 }
                 HookManager.KeyDown += HookManager_KeyDown;
             }
@@ -341,24 +349,32 @@ namespace WindowsFormsApp1
             {
                 if (dht)
                 {
-                    // MessageBox.Show("DHt:"+targetDHT.X.ToString() +"-"+ targetDHT.Y);
+                    Point current = GetCursorPoint();
                     SetCursorPos(targetDHT.X, targetDHT.Y);
-                    //int offset = 5;
-                    //ClipCursorOffset(targetDHT, offset);
                     Thread.Sleep(100);
+                    //Bam DHT
+                    int lanthu = 0;
+                    Bitmap bmp2 = CaptureHelper.CaptureImage(new Size(10, 10), new Point(110, 580));
+                    chaylai:
+                    lanthu++;
+                    if (lanthu > 10)
+                    {
+                        SetCursorPos(current.X, current.Y);
+                        goto boqua;
+                    }
                     HuyKeyPress(KeyDirectX.F2);
-                    Thread.Sleep(100);
-                    HuyKeyPress(KeyDirectX.F2);
-                    Thread.Sleep(100);
-                    HuyKeyPress(KeyDirectX.F2);
-                    //Thread.Sleep(100);
-                    //HuyKeyPress(KeyDirectX.F2);
-                    //Thread.Sleep(100);
+                    Thread.Sleep(50);
+                    Bitmap bmp1 = CaptureHelper.CaptureImage(new Size(10, 10), new Point(110, 580));
+                    //if (true)//save image
+                    //{
+                    //    bmp1.Save(System.IO.Directory.GetCurrentDirectory() + @"\Img\DHT.PNG");
+                    //}
+                    if (CompareBitmapsFast(bmp1, bmp2)) { goto chaylai; }
                     if (targetX2.X != 0 && targetX2.Y != 0)
                     {
                         SetCursorPos(targetX2.X, targetX2.Y);
                     }
-                    //ClipCursor(ref current_rect);
+                    boqua:
                     dht = false;
                 }
                 HuyKeyPress(KeyDirectX.F3);
@@ -366,6 +382,43 @@ namespace WindowsFormsApp1
             }
             while (continuedSpame);
         }
+
+        private bool CompareBitmapsFast(Bitmap bmp1, Bitmap bmp2)
+        {
+            if (bmp1 == null || bmp2 == null)
+                return false;
+            if (object.Equals(bmp1, bmp2))
+                return true;
+            if (!bmp1.Size.Equals(bmp2.Size) || !bmp1.PixelFormat.Equals(bmp2.PixelFormat))
+                return false;
+
+            int bytes = bmp1.Width * bmp1.Height * (Image.GetPixelFormatSize(bmp1.PixelFormat) / 8);
+
+            bool result = true;
+            byte[] b1bytes = new byte[bytes];
+            byte[] b2bytes = new byte[bytes];
+
+            BitmapData bitmapData1 = bmp1.LockBits(new Rectangle(0, 0, bmp1.Width, bmp1.Height), ImageLockMode.ReadOnly, bmp1.PixelFormat);
+            BitmapData bitmapData2 = bmp2.LockBits(new Rectangle(0, 0, bmp2.Width, bmp2.Height), ImageLockMode.ReadOnly, bmp2.PixelFormat);
+
+            Marshal.Copy(bitmapData1.Scan0, b1bytes, 0, bytes);
+            Marshal.Copy(bitmapData2.Scan0, b2bytes, 0, bytes);
+
+            for (int n = 0; n <= bytes - 1; n++)
+            {
+                if (b1bytes[n] != b2bytes[n])
+                {
+                    result = false;
+                    break;
+                }
+            }
+
+            bmp1.UnlockBits(bitmapData1);
+            bmp2.UnlockBits(bitmapData2);
+
+            return result;
+        }
+
         private Point FindToaDoX2(Point current, bool nguoc = false)
         {
             Point select = toadoX2[0];
@@ -473,7 +526,7 @@ namespace WindowsFormsApp1
         public static void HuyKeyPress(KeyDirectX keyCode, int milisecondsLoop = 0)
         {
             DateTime now = DateTime.Now;
-        loop:
+            loop:
             INPUT iNPUT = new INPUT
             {
                 Type = 1u
